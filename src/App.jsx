5c4203dfbedad9175f5e78f4efd6cd51c0590772
Dimitris Kytr. <dkitridis99@gmail.com>
2024-03-06 11:48:47 +0200

    container

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      <main className="mx-auto">
+      <main className="mx-auto 2xl:w-9/12 ">
         <SearcBox />
       </main>
     </div>
