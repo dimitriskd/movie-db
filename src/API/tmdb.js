306e6fcbf8a4dc120064fc58787e02d54b2090c6
Dimitris Kytr. <dkitridis99@gmail.com>
2024-03-06 11:39:28 +0200

    style changes

--- src/API/tmdb.js
+++ src/API/tmdb.js
@@ -11,7 +11,6 @@ export const APIContainer = {
     async searchBox(query){
         try {
             const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, this.config);
-            console.log(response);
             return response;
         } catch (error) {
             console.error(error);
--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -21,30 +21,32 @@ export default function SearcBox() {
         setSearchQuery(query);
         setTimeout(async () => {
 
-        },3000)
+        },1000)
     }
 
     return (
         <section className="parallax bg-blend-multiply">
-            <div className="overlay flex flex-col justify-center items-center relative">
-                <h1 className="absolute md:top-32 md:left-32 top-16 text-white md:text-4xl text-2xl font-bold tracking-wide">Welcome to Movie-DB</h1>
-                <form 
-                    onSubmit={ e => handleForm(e) } 
-                    className="search-box my-32 md:my-44"
-                >
-                    <input 
-                        onChange={ e =>handleChange(e) } 
-                        type="text" name="query" 
-                        className="search w-full text-md p-3 outline-none" 
-                        placeholder="Search for a Movie, TV Show, Person . . ." 
-                    />
-                    <button 
-                        type="submit" 
-                        className="search--button flex items-center text-white bg-ebony-clay-700 p-3"
-                    > 
-                        <span className="material-icons">search</span> 
-                    </button>
-                </form>
+            <div className="overlay flex flex-col justify-center items-center">
+                <div className="flex flex-col justify-center items-center md:items-start my-32 md:my-44 w-5/6 mx-auto">
+                    <h1 className="text-white md:text-4xl text-2xl font-bold tracking-wide mb-2">Welcome to Movie-DB</h1>
+                    <form
+                        onSubmit={ e => handleForm(e) }
+                        className="w-full flex"
+                    >
+                        <input
+                            onChange={ e =>handleChange(e) }
+                            type="text" name="query"
+                            className="search w-full text-md p-3 outline-none"
+                            placeholder="Search for a Movie, TV Show, Person . . ."
+                        />
+                        <button
+                            type="submit"
+                            className="search--button flex items-center text-white bg-ebony-clay-700 p-3"
+                        >
+                            <span className="material-icons">search</span>
+                        </button>
+                    </form>
+                </div>
             </div>
         </section>
     )
