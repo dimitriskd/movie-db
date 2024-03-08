196719b6fe5e1d91f13485d8786015f755f708d4
Dimitris Kytr. <dkitridis99@gmail.com>
2024-03-08 19:25:06 +0200

    added new favicon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import logo from "../assets/logo/logo-line/svg/logo-no-background.svg";
 
-
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,23 +13,21 @@ export default function Navbar() {
       <div className="container px-6 py-3 mx-auto md:flex justify-between">
         <div className="flex items-center justify-between">
           <a href="#">
-            <img
-              className="w-auto h-10 sm:h-7"
-              src={logo}
-              alt="Movie-DB"
-            />
+            <img className="w-auto h-10 sm:h-7" src={logo} alt="Movie-DB" />
           </a>
           <div className="relative ml-8 hidden md:block mt-4 md:mt-0">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                <span className="material-symbols-outlined text-gray-400">search</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="material-symbols-outlined text-gray-400">
+                search
               </span>
+            </span>
 
-              <input
-                type="text"
-                className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600  focus:outline-none focus:ring focus:ring-opacity-80 focus:ring-persian-blue-700"
-                placeholder="Search"
-              />
-            </div>
+            <input
+              type="text"
+              className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600  focus:outline-none focus:ring focus:ring-opacity-80 focus:ring-persian-blue-700"
+              placeholder="Search"
+            />
+          </div>
 
           {/* Mobile menu button */}
           <div className="flex md:hidden transition-all ease-in duration-200">
@@ -42,15 +39,11 @@ export default function Navbar() {
             >
               {/* Open Icon */}
               {!isOpen && (
-                <span className="material-symbols-outlined">
-                  menu
-                </span>
+                <span className="material-symbols-outlined">menu</span>
               )}
               {/* Close Icon */}
               {isOpen && (
-                <span className="material-symbols-outlined">
-                  close
-                </span>
+                <span className="material-symbols-outlined">close</span>
               )}
             </button>
           </div>
@@ -79,7 +72,7 @@ export default function Navbar() {
             </a>
           </div>
         </div>
-        
+
         {/* Mobile Menu open: "block", Menu closed: "hidden" */}
         {isOpen && (
           <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between">
@@ -106,7 +99,9 @@ export default function Navbar() {
 
             <div className="relative mt-4 md:mt-0">
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                <span className="material-symbols-outlined text-gray-400">search</span>
+                <span className="material-symbols-outlined text-gray-400">
+                  search
+                </span>
               </span>
 
               <input
--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,45 +1,19 @@
 import { useEffect, useState } from "react";
 import { APIContainer } from "../API/tmdb";
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
 export default function SearcBox() {
-
-    const [searchQuery, setSearchQuery] = useState("");
-    const [suggestionsList, setSuggestionsList] = useState();
-    const [suggestions, setSuggestions] = useState();
-
-    async function handleForm(e) {
-        e.preventDefault();
-        try {
-            const response = await APIContainer.searchBox(searchQuery);
-            const data = response.data.results;
-            setSuggestionsList(data);
-            console.log(data);
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    function handleChange(e){
-        const query = e.target.value;
-        setSearchQuery(query);
-        setTimeout(async () => {
-
-        },1000)
-    }
-
-    
-
-
-    return (
-        <div className="flex justify-around items-center">
-            <div className="ml-10">
-                <h1 className="text-4xl font-bold">Welcome to Movie-DB.</h1>
-                <h2 className="text-2xl">Your 2nd best source for Movies, TV Shows and People...</h2>
-            </div>
-            <div className="w-2/3">
-                <Spline scene="https://prod.spline.design/WQ5KTeJT-5h28609/scene.splinecode" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex justify-around items-center">
+      <div className="ml-10">
+        <h1 className="text-4xl font-bold">Welcome to Movie-DB.</h1>
+        <h2 className="text-2xl font-semibold">
+          Your 2nd best source for Movies, TV Shows and People...
+        </h2>
+      </div>
+      <div className="w-2/3">
+        <Spline scene="https://prod.spline.design/WQ5KTeJT-5h28609/scene.splinecode" />
+      </div>
+    </div>
+  );
 }
