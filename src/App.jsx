8003ac260f85bf13a8687d92d2236548aa36e3dc
Dimitris Kytr. <dkitridis99@gmail.com>
2024-03-11 23:52:14 +0200

    updated header with search and chanegd navbar a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import Navbar from "./Components/Navbar";
-import SearcBox from "./Components/SearchBox";
+import { useEffect, useState } from "react";
 import "./styles/App.css";
+import { APIContainer } from "./API/tmdb";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
 
 export default function App() {
+  
+  const [popular, setPopular] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const popularMovies = async () => {
+      try {
+        const response = await APIContainer.fetch_popular();
+        const data = response.data.results;
+        setPopular(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    popularMovies();
+  },[])
+
+  if (loading) {
+    // Render a simple loading screen
+    return (
+      <div className="loading-screen">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <nav className="shadow-lg">
         <Navbar />
       </nav>
-      <main className="container py-5 mx-auto md:flex justify-between">
-        {/* <SearcBox /> */}
+      <main className="">
+        <Header movies={popular}/>
       </main>
     </div>
   );
